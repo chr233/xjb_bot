e3dea1eea509bf63183de3da8dde6c0be857e6b0
Chr_ <chr@chrxw.com>
2021-10-31 14:33:19 +0800

    update

--- user/user_info.py
+++ user/user_info.py
@@ -2,7 +2,7 @@
 # @Author       : Chr_
 # @Date         : 2021-10-29 18:12:45
 # @LastEditors  : Chr_
-# @LastEditTime : 2021-10-29 19:46:58
+# @LastEditTime : 2021-10-31 14:27:59
 # @Description  : 
 '''
 
@@ -14,12 +14,6 @@ async def getUserInfo(message: Message):
 
 
 async def myInfo(message: Message):
-    user = message.from_user
-    
-    user_nick = user.full_name
-    user_name = user.mention
-    user_id = user.id
-    
-    print(user)
-    
-    await message.reply(message.from_user.full_name)
+    user = message.user
+        
+    await message.reply(user.user_nick)
